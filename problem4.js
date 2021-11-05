87633b4f5f3f173c9d6a84e9c6dc4634daacc185
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-11-04 21:45:03 -0500

    update.

---
+++ problem4.js
@@ -0,0 +1,19 @@
+//Write a JavaScript function that checks if a single word is a palindrome.
+
+function Palindrome(str) {
+
+	// get the total length of the words
+	const len = str.length
+
+	// Use for loop to divide the words into 2 half
+	for (let i = 0; i < len / 2; i++) {
+
+		// Validate the first and last characters are the same
+		if (str[i] !== str[len - 1 - i]) {
+			alert( "It is not a palindrome");
+		}
+	}
+	alert("It is a palindrome");
+}
+
+console.log(Palindrome("racecar"));
